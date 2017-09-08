b4bc76a524e2e4639dc1cf248da02375fdcbfe25
Robert Balazsi <softrbalazsi@alarm.com>
2017-09-08 16:47:33 +0300

    Set Hibernate dialect, minor code cleanup.

--- search-service/src/main/java/org/webshop/searchservice/repository/ProductRepository.java
+++ search-service/src/main/java/org/webshop/searchservice/repository/ProductRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.webshop.searchservice.entity.Product;
-import org.webshop.searchservice.entity.ProductCategory;
 
 import java.util.List;
 
