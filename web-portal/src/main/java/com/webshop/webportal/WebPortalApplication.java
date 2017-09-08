cf3bd4df5d0f468540d99cea351dfb8809e28330
Robert Balazsi <contact@robertbalazsi.com>
2017-09-09 00:58:25 +0300

    Removed workspace-specific files from Git tracking, added entry point for Web Portal app

---
+++ web-portal/src/main/java/com/webshop/webportal/WebPortalApplication.java
@@ -0,0 +1,12 @@
+package com.webshop.webportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPortalApplication.class, args);
+    }
+}
